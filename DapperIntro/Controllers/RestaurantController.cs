fbd6cb499a1fcbdc03c7c8267c2297f6342be2f2
Phillip Conrad <phillipconrad@rocketmortgage.com>
2021-12-07 14:49:04 -0500

    next commit

---
+++ DapperIntro/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DapperIntro.Models;
+using Dapper;
+
+namespace DapperIntro.Controllers
+{
+    //To make a controller subclass controller
+    //.Net expects your controller name to have the word controller at the end
+    public class RestaurantController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Dish> dishes = new List<Dish>();
+            using(var connect = new MySqlConnection(Secret.connection))
+            {
+                //So using dapper, we are able to put sql queries into a string 
+                //and pass them along to our database
+                //all MySql syntax is useable here
+                string sql = "select * from dishes";
+
+                //open our connection to the database
+                connect.Open();
+                //Expecting to return a single model or a list of models, or nothing
+                dishes = connect.Query<Dish>(sql).ToList();
+
+                //close our connection to a db, if we don't, then the database may not let another method use it
+                connect.Close();
+            }
+
+            return View(dishes[0]);
+        }
+    }
+}
---
+++ DapperIntro/Models/Dish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperIntro.Models
+{
+    public class Dish
+    {
+        //This tells the c# that Id is our primary key in our SQL table
+        //that helps c# with validation
+        [Key]
+        public int id { get; set; }
+        [MaxLength(20)]
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsHealthy { get; set; }
+        [Range(1,10)]
+        public int SpiceLevel { get; set; }
+    }
+}
